6c1ea649041587c559d725a8ff02cc5c5ef3f630
whoisxnio <enioblvck@gmail.com>
2023-03-23 12:40:26 +0100

    add repository pattern

---
+++ PokemonReviewApp/Controllers/PokemonController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Model;
+
+namespace PokemonReviewApp.Controllers
+{
+    [Route("api/[controller]")]
+    public class PokemonController : Controller
+    {
+        private readonly IPokemonRepository _pokemonRepository;
+        public PokemonController(IPokemonRepository pokemonRepository)
+        {
+            _pokemonRepository = pokemonRepository;
+        }
+
+        [HttpGet]
+        [ProducesResponseType(200, Type = typeof(IEnumerable<Pokemon>))]
+        public IActionResult getPokemons() 
+        {
+            var pokemons = _pokemonRepository.GetPokemons();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(pokemons);
+        }
+    }
+}
---
+++ PokemonReviewApp/Interfaces/PokemonRepository.cs
@@ -0,0 +1,19 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Model;
+
+namespace PokemonReviewApp.Interfaces
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context;
+        public PokemonRepository(DataContext context) 
+        { 
+            _context = context;
+        }
+
+        public ICollection<Pokemon> GetPokemons() 
+        { 
+            return _context.Pokemon.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
