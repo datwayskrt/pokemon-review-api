1ad96c231e17766bce232f559f99eea1e0db231d
whoisxnio <enioblvck@gmail.com>
2023-03-23 19:39:29 +0100

    automapper bug fixed

--- PokemonReviewApp/Controllers/OwnerController.cs
+++ PokemonReviewApp/Controllers/OwnerController.cs
@@ -30,5 +30,39 @@ namespace PokemonReviewApp.Controllers
 
             return Ok(owners);
         }
+
+        [HttpGet("{ownerId}")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+
+            var owner = _mapper.Map<OwnerDto>(_ownerRepository.GetOwner(ownerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(owner);
+        }
+
+        [HttpGet("{ownerId}/pokemon")]
+        [ProducesResponseType(200, Type = typeof(Owner))]
+        [ProducesResponseType(400)]
+        public IActionResult GetPokemonByOwner(int ownerId)
+        {
+            if (!_ownerRepository.OwnerExists(ownerId))
+                return NotFound();
+
+            var owner = _mapper.Map<List<PokemonDto>>(_ownerRepository.GetPokemonByOwner(ownerId));
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(owner);
+
+        }
+
     }
 }
