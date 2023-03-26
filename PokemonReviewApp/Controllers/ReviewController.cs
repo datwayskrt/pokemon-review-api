7b274c5a45618fab522183f65542a01aac50b951
whoisxnio <enioblvck@gmail.com>
2023-03-26 17:00:01 +0100

    review update done

--- PokemonReviewApp/Controllers/ReviewController.cs
+++ PokemonReviewApp/Controllers/ReviewController.cs
@@ -103,5 +103,34 @@ namespace PokemonReviewApp.Controllers
             return Ok("Successfully created");
         }
 
+        [HttpPut("{reviewId}")]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(404)]
+        public IActionResult UpdateReview(int reviewId, [FromBody] ReviewDto updateReview)
+        {
+            if (updateReview == null)
+                return BadRequest(ModelState);
+
+            if (reviewId != updateReview.Id)
+                return BadRequest(ModelState);
+
+            if (!_reviewerRepository.ReviewerExists(reviewId))
+                return NotFound();
+
+            if (!ModelState.IsValid)
+                return BadRequest();
+
+            var reviewMap = _mapper.Map<Review>(updateReview);
+
+            if (!_reviewRepository.UpdateReview(reviewMap))
+            {
+                ModelState.AddModelError("", "Something went wrong updating reviewer");
+                return StatusCode(50, ModelState);
+
+
+            }
+            return NoContent();
+        }
     }
 }
